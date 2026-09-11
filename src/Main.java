8ee97bfcb70a0ede9feeefc6dc744d2113099408
Xavier LLuen <xavieryt103@gmail.com>
2026-09-11 18:33:30 -0500

    Agregando la solucion de Sistema de pedidos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import Clientes.GestordeClientes;
+import Pedidos.GestorDePedidos;
+import Productos.GestorProductos;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        GestordeClientes gestorClientes = new GestordeClientes();
+        gestorClientes.registrarCliente("Juan", "Pérez", "juan@example.com");
+        gestorClientes.registrarCliente("María", "López", "maria@example.com");
+
+
+
+        GestorDePedidos gestor = new GestorDePedidos();
+        gestor.registrarPedido("Juan Pérez", "Calle Falsa 123");
+        gestor.consultarPedido("Juan Gomez", "Calle Falsa 124");
+
+        GestorProductos gestorProductos = new GestorProductos();
+        gestorProductos.registrarProducto("Producto A", 10.99);
+        gestorProductos.consultarProducto("Producto B", 10.99);
+
+        
+
     }
 }
